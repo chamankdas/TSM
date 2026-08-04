c039e8e4091b271530ccda319482cf74449876d2
chamankdas <chamandas254@gmail.com>
2026-08-04 11:48:18 +0530

    Revert "fix: add the return in SignIn class"
    This reverts commit be6e6abf6065b73a889b93d2bc8ca0041f1c2f98.

--- tsm-f/src/components/login.jsx
+++ tsm-f/src/components/login.jsx
@@ -1,32 +1,10 @@
-import React from 'react'
+import { Component } from 'react'
 import '../App.css'
 
 
-// function SignIn(){
-//     return(
-//        <div className='container'>
-//             <div className='bo'>
-//                 <h2>Login</h2>
-//                 <div className="fi">
-//                     <div>
-//                         <label htmlFor="">Username</label>
-//                         <input type="text" name="" id="" className="h1" />
-//                     </div>
-//                     <div>
-//                         <label htmlFor="">passwored</label>
-//                         <input type="password" name="" id="" className="h1"/>
-//                     </div>
-//                     <input type="submit" value="Login" id="but" />
-//                 </div>
-//             </div>
-//         </div>
-//     )
-// }
-
-class SignIn extends React.Component{
+class SignIn extends Component{
     render() {
-        return(
-        <div className='container'>
+       <div className='container'>
             <div className='bo'>
                 <h2>Login</h2>
                 <div className="fi">
@@ -36,12 +14,13 @@ class SignIn extends React.Component{
                     </div>
                     <div>
                         <label htmlFor="">passwored</label>
-                        <input type="password" name="" id="" className="h1" />
+                        <input type="password" name="" id="" className="h1"/>
                     </div>
                     <input type="submit" value="Login" id="but" />
                 </div>
             </div>
-        </div>)
+        </div>
     }
 }
+
 export default SignIn;
