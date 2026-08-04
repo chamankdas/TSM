2831e3d0018af52120f9a79e024687b4868d7b0f
chamankdas <chamandas254@gmail.com>
2026-08-04 11:50:06 +0530

    Fix: add return in SignIn class in login component

--- tsm-f/src/components/login.jsx
+++ tsm-f/src/components/login.jsx
@@ -3,23 +3,25 @@ import '../App.css'
 
 
 class SignIn extends Component{
-    render() {
-       <div className='container'>
-            <div className='bo'>
-                <h2>Login</h2>
-                <div className="fi">
-                    <div>
-                        <label htmlFor="">Username</label>
-                        <input type="text" name="" id="" className="h1" />
+    render() { 
+        return (
+            <div className='container'>
+                <div className='bo'>
+                    <h2>Login</h2>
+                    <div className="fi">
+                        <div>
+                            <label htmlFor="">Username</label>
+                            <input type="text" name="" id="" className="h1" />
+                        </div>
+                        <div>
+                            <label htmlFor="">passwored</label>
+                            <input type="password" name="" id="" className="h1" />
+                        </div>
+                        <input type="submit" value="Login" id="but" />
                     </div>
-                    <div>
-                        <label htmlFor="">passwored</label>
-                        <input type="password" name="" id="" className="h1"/>
-                    </div>
-                    <input type="submit" value="Login" id="but" />
                 </div>
             </div>
-        </div>
+        );
     }
 }
 
